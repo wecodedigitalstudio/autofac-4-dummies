708f18de117a9d0653db8aa4aa3ae65ad0bacd0d
Roberto Borsini <roberto.borsini@gmail.com>
2020-04-27 17:28:42 +0200

    getting started

---
+++ Example_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using Autofac.Core;
+using DemoApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_1
+{
+    public class Program
+    {
+        private static IContainer Container { get; set; }
+
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>();
+            Container = builder.Build();
+
+            // The WriteDate method is where we'll make use
+            // of our dependency injection. We'll define that
+            // in a bit.
+            WriteDate();
+        }
+
+        public static void WriteDate()
+        {
+            // Create the scope, resolve your IDateWriter,
+            // use it, then dispose of the scope.
+            using (var scope = Container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();
+            }
+        }
+
+    }
+}
